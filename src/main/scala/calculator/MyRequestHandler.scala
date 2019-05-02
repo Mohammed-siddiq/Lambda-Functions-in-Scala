1bfeba1f117f0d41ba3e3599affe7bfe8c1e203b
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-05-01 23:27:15 -0500

    Added Rest request support in the lambda

--- src/main/scala/calculator/MyRequestHandler.scala
+++ src/main/scala/calculator/MyRequestHandler.scala
@@ -8,17 +8,15 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import scala.collection.JavaConverters._
 import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.lambda.runtime.LambdaLogger
 
 
 class MyRequestHandler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] {
 
 
-  override def handleRequest(request: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
-
+  def runGrpcCalculator(request: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
     val lambdaLogger = context.getLogger
 
-    lambdaLogger.log("Request received...")
+    lambdaLogger.log("GRPC request....")
 
     val decodedString = Base64.getDecoder.decode(request.getBody)
     val inputProto = Input.parseFrom(decodedString)
@@ -51,8 +49,64 @@ class MyRequestHandler extends RequestHandler[APIGatewayProxyRequestEvent, APIGa
 
     response.setIsBase64Encoded(true)
     response.setHeaders(Map("Content-Type" -> "application/grpc+proto").asJava)
+    return response
+  }
 
+  def runRestCalculator(request: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
+
+    val lambdaLogger = context.getLogger
+
+    lambdaLogger.log("REST request....")
+
+    val inputParams = request.getQueryStringParameters()
+    val op1 = inputParams.get("op1").toDouble
+    val op2 = inputParams.get("op2").toDouble
+    val operator = inputParams.get("operator")
+
+    lambdaLogger.log("Request Query Params")
+    lambdaLogger.log("----------------------")
+    lambdaLogger.log(inputParams.toString)
+    lambdaLogger.log("----------------------")
+
+    val responseValue = operator match {
+      case "+" => op1 + op2
+      case "-" => op1 - op2
+      case "*" => op1 * op2
+      case "/" => op1 / op2
+      case _ => "Error : Format : op1 operator op2"
+    }
+
+    lambdaLogger.log("Returning JSON....")
+
+
+    lambdaLogger.log("-------------------------")
+    val response = new APIGatewayProxyResponseEvent
+    response.setBody(
+      s"""{
+         |"inputExpression" : "${op1.toString} $operator $op2",
+         |"output" : $responseValue,
+         |"message" : success
+         }
+      """.stripMargin)
+
+    response.setHeaders(Map("Content-Type" -> "application/json").asJava)
     return response
 
   }
+
+  override def handleRequest(request: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
+
+    context.getLogger.log("Request received...")
+
+    context.getLogger.log("Identifying request type....")
+
+    return request.getHeaders.get("Content-Type") match {
+      case "application/grpc+proto" => runGrpcCalculator(request, context)
+      case _ => runRestCalculator(request, context)
+    }
+
+
+  }
+
+
 }
